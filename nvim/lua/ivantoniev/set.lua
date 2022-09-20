4bfa619e12da3acc9f12f73a28009bf4e3ccb690
Ivan Ivanov <ivantoniev@gmail.com>
2022-09-20 19:38:28 +0100

    Remove trailing space

--- nvim/lua/ivantoniev/set.lua
+++ nvim/lua/ivantoniev/set.lua
@@ -30,7 +30,7 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 -- Tabs vs spaces
-vim.opt.tabstop = 2 
+vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
