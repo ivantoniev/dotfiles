7cb6a379a57e908f58fa9f381ce2003e9a945429
Ivan Ivanov <ivantoniev@gmail.com>
2022-09-20 19:31:27 +0100

    Switch lines around for nvim theme

--- nvim/after/plugin/theming.lua
+++ nvim/after/plugin/theming.lua
@@ -1,5 +1,6 @@
+vim.cmd("colorscheme tokyonight")
+
 vim.g.tokyonight_transparent_siderbar = true
 vim.g.tokyonight_transparent = false
 vim.opt.background = "dark"
 
-vim.cmd("colorscheme tokyonight")
